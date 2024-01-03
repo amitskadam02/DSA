55734a357f2bbb786ba16572154c72c5f25ac906
amitskadam02 <72244881+amitskadam02@users.noreply.github.com>
2024-01-03 09:00:59 +0530

    Basics of programming level 1
    First C++ program

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // /// cout<<"Amit Kadam"<<endl;
+  // // cout<<"HI\n";
+  // cout<<"AMit";cout<<"Kadam";
+  int num = 50;
+
+  cout << "";
+  cout << sizeof(num) << endl;
+}
