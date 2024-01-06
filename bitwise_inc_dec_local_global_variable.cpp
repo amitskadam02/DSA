37cc79159ed2f1c2cda74357373c84dcf1f2188a
amitskadam02 <72244881+amitskadam02@users.noreply.github.com>
2024-01-06 13:45:29 +0530

    bitwise_inc_dec_local_global_variable

---
+++ bitwise_inc_dec_local_global_variable.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+using namespace std;
+
+//global variable
+int age=20; //BAD PRACTICE
+
+int main()
+{
+    // AND
+    // int a=2;
+    // int b=3;
+
+    // cout<<(a&b)<<endl;
+
+    // // OR "|"
+    // int a=5;
+    // int b=10;
+
+    // cout<<(a|b)<<endl;
+
+    // // NOT "~"
+    // int num=1;
+    // cout<<~(num)<<endl;
+    // cout<<(~num)<<endl;
+    // cout<<~num<<endl;
+
+    // // XOR "^"
+    // int a = 5;
+    // int b = 10;
+
+    // cout<<(a^b)<<endl;
+
+    // int a=7;
+    // int ans=(a<<4);
+    // cout<<ans<<endl;
+
+    // unsigned int n=-100;
+    // int ans= (n>>1);
+    // cout<<ans;
+
+    // int n=10;
+    // cout<<(n<<-1);
+
+    // // PRE POST
+    // int a=20;
+    // cout<<++a<<endl;//21
+    // cout<<a++<<endl;//21
+    // cout<<a<<endl;//22
+
+    // int a=10;
+
+    // cout<<(++a)*10<<endl;
+    // cout<<(a++)*10<<endl;
+    // cout<<a<<endl;
+
+    // int a=10;
+
+    // cout<<((++a)*(a++))<<endl;
+
+    // BREAK AND continue
+
+    // for(int i=0;i<5;i++){
+    //     cout<<i<<endl;
+    //     if(i==3)
+    //     break;// it will skip all next iteration and come out of loop
+    // }
+
+    // for(int i=0;i<=5;i++){
+    //     cout<<"HI AMIT"<<endl;
+    //     continue; // this will skipped next part
+    //     cout<<"HI THERE"<<endl;
+    // }
+
+
+     // Local and Global variable
+    // int a=5;
+    // int b;
+
+    // a=25;
+    // cout<<a<<endl; 
+
+    // for(int i=0;i<5;i++){
+
+    // }
+    // cout<<i<<endl; // iis not accesible outside of scope
+
+    // if(true){
+    //     int a=20;
+    //     cout<<a<<endl;
+
+    // }
+    // cout<<a<<endl;//can not access
+
+
+    // int a=100;
+    // if(true){
+    //     cout<<"inside if number 1"<<endl;
+    //     int a=101;
+    //     if(true){
+    //         cout<<"inside if 2"<<endl;
+    //         cout<<a<<endl;
+    //     }
+
+    // }
+
+
+
+
+
+    return 0;
+}
