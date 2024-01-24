a4a5f54c9c552d1259a9e9bf5ab688d064876651
amitskadam02 <72244881+amitskadam02@users.noreply.github.com>
2024-01-24 19:13:15 +0530

    Array debug

---
+++ Array_debug.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// 2.
+//  int linearSearch(int arr[], int n ,int val){
+//      for(int i=0;i<n;i++){
+//          if(arr[i]==val){
+//              return i;
+//          }
+//      }
+//      return -1;
+//  }
+
+// 1
+//  int main() {
+//    int n, sum;
+//    cin >> n;
+//    int input[n];
+//    for (int i = 0; i < n; i++) {
+//      cin >> input[i];
+//    }
+//    for (int i = 0; i < n; i++) {
+//      sum = sum + input[i];
+//    }
+//    cout << sum << endl;
+//    return 0;
+//  }
