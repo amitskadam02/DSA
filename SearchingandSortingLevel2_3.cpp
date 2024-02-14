b3642f5c1675e9598a7fb0a5b5c0b5de53139ce9
amitskadam02 <72244881+amitskadam02@users.noreply.github.com>
2024-02-14 23:06:22 +0530

    SearchingandSortingLevel2_3.cpp_find_pivot_element

---
+++ SearchingandSortingLevel2_3.cpp
@@ -0,0 +1,14 @@
+// find pivot element
+
+int search(int arr[], int target) {
+  int pivotindex = findpivotindex(nums);
+  int n = arr.size();
+  int ans = -1;
+  // search in A
+  if (target >= arr[0] && target <= arr[pivotindex]) {
+    ans = binarysearch(nums, 0, pivotindex, target);
+  } else {
+    ans = binarysearch(nums, pivotindex + 1, n - 1, target);
+  }
+  return ans;
+};
