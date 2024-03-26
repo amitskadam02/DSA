ee772af5a6b46c8bf405edeeff3785cbb3a5b600
amitskadam02 <72244881+amitskadam02@users.noreply.github.com>
2024-03-26 19:17:27 +0530

    String length and reverse

--- CharArrayandStringsClass1_3.cpp
+++ CharArrayandStringsClass1_3.cpp
@@ -1,19 +1,52 @@
-
-//
+// Length of string and reverse string
 #include <iostream>
+#include <string.h>
 
 using namespace std;
+
+int findLength(char ch[], int size) {
+
+  //
+  int length = 0;
+
+  for (int i = 0; i < size; i++) {
+    if (ch[i] == '\0') {
+      break;
+    } else {
+      length++;
+    }
+  }
+
+  return length;
+}
+
+void reverseSting(char ch[], int n) {
+  int i = 0;
+  int j = n - 1;
+  while (i <= j) {
+    swap(ch[i], ch[j]);
+    i++;
+    j--;
+  }
+}
 
 int main() {
 
   // creation
   char ch[10];
 
-  // input
-  // cin>>ch;
-  cin.getline(ch, 100); // sentence as a input(space, tab, enter)
-  /////
+  cout << "Enter your string:" << endl;
 
-  // output
-  cout << "Printing the value: " << ch << endl;
+  //   input
+  //   cin>>ch; // string (word)
+  cin.getline(ch, 100); // string (sentence)
+
+  int len = findLength(ch, 100);
+  cout << "Lenth of string is:" << len << endl;
+
+  cout << "Befor reverse :" << ch << endl;
+  reverseSting(ch, len);
+  cout << "After reverse :" << ch << endl;
+
+  return 0;
 }
