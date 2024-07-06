7f8f5330c118b8cba34fd9a5197b0e2ac9ca72b7
amitskadam02 <72244881+amitskadam02@users.noreply.github.com>
2024-07-06 18:03:44 +0530

    Pointers1.4

---
+++ Pointers1.4.cpp
@@ -0,0 +1,16 @@
+// Pointers Class 1.4
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  // int *ptr;
+  // int x;
+  // ptr = ptr + 1;
+
+  char ch;
+  char cptr = &ch;
+  cout << cptr
+}
