ca77e384e68c0fcfcce761d06634dee9946e20e3
Amit Kadam <72244881+amitskadam02@users.noreply.github.com>
2024-12-03 08:17:17 +0530

    Recursion3.2
    Subsequence os string

--- main.cpp
+++ main.cpp
@@ -1,11 +1,47 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-  // /// cout<<"Amit Kadam"<<endl;
-  // // cout<<"HI\n";
-  // cout<<"AMit";cout<<"Kadam";
-  int num = 50;
 
-  cout << "";
-  cout << sizeof(num) << endl;
+//subsequence in string
+void findSubsequence(string str, string output, int index)
+{
+  // base case
+  if(index >= str.length())
+  {
+    // we build the output in output
+    cout<<"--> "<<output<<endl;
+    return;
+  }
+  
+  // //**************************************************
+  // // Solution1: Considering exclude first
+  // // exclude
+  // findSubsequence(str, output, index+1);
+
+  // // include
+  // output.push_back(str[index]);
+  // findSubsequence(str, output, index+1);
+  // //**************************************************
+
+  //**************************************************
+  // Solution2: Considering Include first
+  // include
+  output.push_back(str[index]);
+  findSubsequence(str, output, index+1);
+
+  // include
+  output.pop_back();
+  findSubsequence(str, output, index+1);
+  //**************************************************
+
+}
+
+int main()
+{
+  string str = "abc";
+  string output = "";
+  int index = 0;
+
+  findSubsequence(str, output, index);
+
+  return 0;
 }
