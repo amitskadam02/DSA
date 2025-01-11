9aaea54e7a25cdcfe2f029b276198c9124eeb95f
Amit Kadam <72244881+amitskadam02@users.noreply.github.com>
2025-01-11 23:14:14 +0530

    Tree1.1

---
+++ Tree1.1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+// #include<>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    int* left;
+    int* right;
+    
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL; 
+    }
+    
+    Node* createTree(){
+        
+    }
+    
+};
+int main(){
+    
+}
