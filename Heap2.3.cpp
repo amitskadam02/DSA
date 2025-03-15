1f5c85b0b4b7bc911ef89589e16b71bc26bf21c8
Amit Kadam <72244881+amitskadam02@users.noreply.github.com>
2025-03-15 10:47:05 +0530

    Heap2.3

--- Heap2.3.cpp
+++ Heap2.3.cpp
@@ -20,13 +20,31 @@ int kthSmallestElement(int arr[], int size, int k){
     return pq.top();
     
 }
+
+int kthGreatestElement(int arr[], int size, int k){
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for(int i=0; i<k; i++){
+        int element = arr[i];
+        pq.push(element);
+    }
+    
+    for(int i=k; i<size; i++){
+        int element = arr[i];
+        if(element > pq.top())
+        {
+            pq.pop();
+            pq.push(element);
+        }
+    }
+    return pq.top();
+}
 int main() {
-    // Write C++ code here
-    //std::cout << "Try programiz.pro";
     int arr[] = {1,2,3,4,5,6,7,8,9,10};
     int size = 10;
-    int k = 4;
-    cout<< kthSmallestElement(arr, size,k);
+    int k = 2;
+    //cout<< "Kth smallest element un array:"<<kthSmallestElement(arr, size,k)<<endl;
+    cout<< "Kth largest element un array:"<<kthSmallestElement(arr, size,k)<<endl;
 
     return 0;
 }
