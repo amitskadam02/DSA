f6afbf7370bf546ef05345fa09cfb996957bbb0b
Amit Kadam <72244881+amitskadam02@users.noreply.github.com>
2025-04-14 22:05:09 +0530

    Car

---
+++ Car.cpp
@@ -0,0 +1,35 @@
+class Car{
+	public:
+	void ignitionStart(){
+		cout<<"Ignition On"<<endl;
+	}
+	
+	void ignitionOff(){
+		cout<<"Ignition Off"<<endl;
+	}
+	
+	
+	protected:
+
+	private:
+	int a;
+	int b;
+
+Car(){
+
+}
+
+~Car(){
+
+}
+
+};
+
+Class
+
+0200
+0000 0010 0000 0000
+ 
+0300
+0000 0011 0000 0000
+
